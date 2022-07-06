4312cbc99a22778057963a56b4b3b974346bfa9f
Vish Pepala <vish@claz.org>
2022-07-06 11:11:42 +0530

    Initial Commit

--- src/main/java/com/application/cources/CoursesController.java
+++ src/main/java/com/application/cources/CoursesController.java
@@ -1,14 +1,9 @@
 package com.application.cources;
 
-import com.application.cources.models.Course;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletResponse;
-import java.util.List;
 
 @Controller
 @RequestMapping("/courses")
