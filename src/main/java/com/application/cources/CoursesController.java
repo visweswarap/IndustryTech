c28a614ececf79f35f716bbcf36cb8b6071e3ceb
Vish Pepala <vish@claz.org>
2022-07-06 10:55:35 +0530

    Initial Commit

--- src/main/java/com/application/cources/CoursesController.java
+++ src/main/java/com/application/cources/CoursesController.java
@@ -2,17 +2,20 @@ package com.application.cources;
 
 import com.application.cources.models.Course;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
-@Controller("courses")
+@Controller
+@RequestMapping("/courses")
 public class CoursesController {
 
-    @GetMapping("")
-    String getAllCourses(HttpServlet req, HttpServletResponse res){
+    @GetMapping("/all")
+    String getAllCourses(ModelMap modelMap){
 
         return "success";
     }
