bd80fc23c9e42a998d82e43e29f0da92bde0eed4
Vish Pepala <vish@claz.org>
2022-07-18 10:02:20 +0530

    User details & DAO

--- src/main/java/com/industrytech/users/controller/UserController.java
+++ src/main/java/com/industrytech/users/controller/UserController.java
@@ -11,6 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.sql.SQLException;
 import com.industrytech.database.ConnectionFactory;
 import com.industrytech.users.model.User;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequestMapping("/user")
 public class UserController {
@@ -18,16 +22,17 @@ public class UserController {
     @Autowired
     UserDao userDao;
 
+    @Autowired
+    HttpServletRequest request;
+
     @GetMapping("")
     String getUser(ModelMap modelMap) throws SQLException {
+
+        HttpSession session = request.getSession();
+        User user = new User();
+        user.setEmail("");
+        session.setAttribute("user", user);
         modelMap.addAttribute("message", "");
         return "login";
-     /*  boolean isSaved = userDao.save(user);
-        if (isSaved) {
-            modelMap.addAttribute("message", "SUCCESS FULLY LOGIN");
-        } else {
-            modelMap.addAttribute("message", "FAILED");
-        }
-        return "courses";*/
     }
 }
